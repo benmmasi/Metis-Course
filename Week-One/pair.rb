724df8724ef301345aa6fbc167aa1fb52a0146a0
benmmasi <ben.m.masi@gmail.com>
2014-09-18 14:41:49 -0400

    Creating a hash class.

---
+++ Week-One/pair.rb
@@ -0,0 +1,13 @@
+class Pair
+	def initialize(key, value)
+		@key = key
+		@value = value
+	end
+
+	attr_reader :key
+	attr_accessor :value
+	#def value=(new_value)
+	#	@value = new_value
+	#end
+end
+
