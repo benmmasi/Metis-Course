95edbe713e06ed0e076c2db7f96ae1a386cfab1d
benmmasi <ben.m.masi@gmail.com>
2014-09-19 13:43:09 -0400

    Changes to programs in first week

--- Week-One/flashcard_game.rb
+++ Week-One/flashcard_game.rb
@@ -1,30 +1,48 @@
 class FlashcardGame
+	def initialize(decks)
+		@decks = decks 
+	end
 
 	def play
-		say_hi_to_user
 		loop do
-		 puts "Please select a deck"
-		 deck = gets.chomp
+			list_decks
+			deck = get_selected_deck
 
-		 if deck == ""
-		 	puts "would exit"
-		 	break
-
-		 else
-		 	#deck.play
-		 	#print "would play #{deck}"
-		 end
+			if deck 
+				deck.play
+			else
+		 		puts "Exit"
+		 	end
 		end
 	end
-		private
 
-		def say_hi_to_user
-			puts "Hello, and welcome to my game"
+	private
+
+	def list_decks
+		@decks.each do |deck|
+			puts deck.name
 		end
-	
+	end
+
+	def get_selected_deck
+		puts "Please choose one of the following decks"
+		deck_name = gets.chomp
+		selected_deck = @decks.find { |deck| deck_name == deck.name }
+	end
 end
 
+#decks = []
+
+#spanish_cards = []
+#spanish_cards << Card.new({front:"Gato", back:"Cat"})
+#spanish_cards << Card.new({front:"Perro", back:"Dog"})
+#decks << Deck.new({name:"Spanish", cards:spanish_cards})
+
+#japanese_cards = []
+#japanese_cards << Card.new({front:"Neko", back:"Cat"})
+#japanese_cards << Card.new({front:"Inu", back:"Dog"})
+#decks << Deck.new({name:"Japanese", cards:japanese_cards})
 
 
-flashcard_game = FlashcardGame.new
-flashcard_game.play
+
+
